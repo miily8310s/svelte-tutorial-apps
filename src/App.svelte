7c42b9a6bffb5cb48cc6b5ea9ac9c0112056b6f3
miily8310s <rillakerika@gmail.com>
2021-04-23 08:29:51 +0900

    add tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -11,30 +11,14 @@
     Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
     how to build Svelte apps.
   </p>
-  <p><a href="#/simple">Simple App</a></p>
-  <p><a href="#/">Top</a></p>
+  <p class=""><a href="#/simple">Simple App</a></p>
+  <p><a href="/">Top</a></p>
 </main>
 
 <Router {routes} />
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
