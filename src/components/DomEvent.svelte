fda2d5a848de0dce3eb89a1b1ff10c9250f9ab87
miily8310s <rillakerika@gmail.com>
2021-04-27 07:57:07 +0900

    add dom forward

---
+++ src/components/DomEvent.svelte
@@ -0,0 +1,3 @@
+<button class="mt-2 bg-green-400 hover:bg-green-700 text-white" on:click>
+  Click me
+</button>
--- src/routes/EventForwardApp.svelte
+++ src/routes/EventForwardApp.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   import EventDispatcher from '../components/EventDispatcher.svelte';
+  import DomEvent from '../components/DomEvent.svelte';
   function handleMessage(event) {
     alert(event.detail.text);
+  }
+  function handleDomClick() {
+    alert('Hello Svelte!');
   }
 </script>
 
 <EventDispatcher on:message={handleMessage} />
+<DomEvent on:click={handleDomClick} />
