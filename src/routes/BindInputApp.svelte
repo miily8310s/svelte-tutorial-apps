e37b0c117ce33ce9786e50a34febaa410a29216e
miily8310s <rillakerika@gmail.com>
2021-04-28 08:00:09 +0900

    add group bind

--- src/routes/BindInputApp.svelte
+++ src/routes/BindInputApp.svelte
@@ -3,6 +3,10 @@
   let a = 1;
   let b = 2;
   let checked = false;
+
+  let selectedNumber = 1;
+  let selectedMenu = ['pizza'];
+  let menu = ['pizza', 'pasta', 'macaroni'];
 </script>
 
 <h1 class="font-semibold">Text Inputs</h1>
@@ -25,5 +29,36 @@
     <p>Checked!!!</p>
   {:else}
     <p>No Check...</p>
+  {/if}
+</div>
+
+<h1 class="font-semibold">Group Inputs</h1>
+<div class="border-red-900 border-2 p-2 mt-2">
+  <label>
+    <input type="radio" bind:group={selectedNumber} value={1} />
+    One scoop
+  </label>
+
+  <label>
+    <input type="radio" bind:group={selectedNumber} value={2} />
+    Two scoops
+  </label>
+
+  <label>
+    <input type="radio" bind:group={selectedNumber} value={3} />
+    Three scoops
+  </label>
+  {#each menu as flavour}
+    <label>
+      <input type="checkbox" bind:group={selectedMenu} value={flavour} />
+      {flavour}
+    </label>
+  {/each}
+  {#if selectedMenu.length === 0}
+    <p>No selected one menu...</p>
+  {:else if selectedMenu.length > selectedNumber}
+    <p>Can't select more menu</p>
+  {:else}
+    <p>You ordered {selectedMenu}</p>
   {/if}
 </div>
