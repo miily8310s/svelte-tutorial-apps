920e1bea1fd4cb67c81f21431a7d013834355397
miily8310s <rillakerika@gmail.com>
2021-04-30 08:19:19 +0900

    comment out subscribe method

--- src/routes/StoreApp.svelte
+++ src/routes/StoreApp.svelte
@@ -2,14 +2,18 @@
   import { count } from '../store';
   import StoreButton from '../components/StoreButton.svelte';
 
-  let count_value;
-
-  const countChangeEvent = count.subscribe((value) => {
-    count_value = value;
-  });
+  // only use subscribe methods
+  // let count_value;
+  //
+  // const countChangeEvent = count.subscribe((value) => {
+  // count_value = value;
+  // });
 </script>
 
+<h1 class="mb-3">Writable App</h1>
 <div class="flex gap-2">
-  <h1 class="bg-gray-100 py-1 px-2">{count_value}</h1>
+  <!-- only use subscribe methods -->
+  <!-- <h1 class="bg-gray-100 py-1 px-2">{count_value}</h1> -->
+  <h1 class="bg-gray-100 py-1 px-2">{$count}</h1>
   <StoreButton />
 </div>
