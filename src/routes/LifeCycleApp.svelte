6680cb8f8d50dac5388b0e57544477cdaaa5ebf0
miily8310s <rillakerika@gmail.com>
2021-04-29 14:23:17 +0900

    add tick event component

--- src/routes/LifeCycleApp.svelte
+++ src/routes/LifeCycleApp.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
+  import { tick } from 'svelte';
+
   import MountUp from '../components/MountUp.svelte';
+
+  let text = 'Lorem ipsum dolor sit amet.';
+
+  async function handleKeydown(e: KeyboardEvent) {
+    if (e.key !== 'Tab') return;
+    e.preventDefault();
+
+    const { selectionStart, selectionEnd, value } = this;
+
+    const selection: string = value.slice(selectionStart, selectionEnd);
+
+    const replacement: string = /[a-z]/.test(selection)
+      ? selection.toUpperCase()
+      : selection.toLowerCase();
+
+    text =
+      value.slice(0, selectionStart) + replacement + value.slice(selectionEnd);
+
+    await tick();
+    this.selectionStart = selectionStart;
+    this.selectionEnd = selectionEnd;
+  }
 </script>
 
 <MountUp />
+
+<h1 class="font-semibold">tick</h1>
+<textarea value={text} on:keydown={handleKeydown} />
