6c99e7b9c4e9684b4251856089257d46382a3ced
miily8310s <rillakerika@gmail.com>
2021-04-30 09:04:38 +0900

    add custom store app

--- src/routes/StoreApp.svelte
+++ src/routes/StoreApp.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { count, time, timeProgress } from '../store';
+  import { count, time, timeProgress, num } from '../store';
   import StoreButton from '../components/StoreButton.svelte';
 
   // only use subscribe methods
@@ -31,5 +31,24 @@
   <div class="flex gap-2 flex-col">
     <h1 class="bg-gray-100 py-1 px-2">Now Time is {formatter.format($time)}</h1>
     <p>Open for {$timeProgress} {$timeProgress > 1 ? 'seconds' : 'second'}</p>
+  </div>
+</div>
+<div class="py-2 px-3 border-2 border-red-900 mb-3">
+  <h1 class="mb-3">Custom Store App</h1>
+  <div class="flex gap-2 flex-col">
+    <h1 class="bg-gray-100 py-1 px-2">
+      The number is <input
+        type="number"
+        bind:value={$num}
+        class="text-center"
+      />
+    </h1>
+    <div class="flex flex-row gap-2">
+      <button class="py-1 px-2 bg-gray-100" on:click={num.doubled}>x 2</button>
+      <button class="py-1 px-2 bg-gray-100" on:click={num.taxed}>x 1.25</button>
+      <button class="py-1 px-2 bg-gray-100" on:click={num.initialized}
+        >number reset</button
+      >
+    </div>
   </div>
 </div>
--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 import { writable } from 'svelte/store';
 import { readable, derived } from 'svelte/store';
 
+// writable method
 export const count = writable(0);
 
+// readble/derived method
 export const time = readable(new Date(), function start(set) {
   const interval = setInterval(() => {
     set(new Date());
@@ -18,3 +20,16 @@ const start = new Date();
 export const timeProgress = derived(time, ($time) =>
   Math.round(($time - start) / 1000),
 );
+
+// custom store
+function createNumber() {
+  const { subscribe, set, update } = writable(2);
+  return {
+    subscribe,
+    doubled: () => update((n) => n * 2),
+    taxed: () => update((n) => n * 1.1),
+    initialized: () => set(2),
+  };
+}
+
+export const num = createNumber();
