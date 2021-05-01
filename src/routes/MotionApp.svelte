a9c080e21a9b86f7dcfc31bc7cf91b3ef195946d
miily8310s <rillakerika@gmail.com>
2021-05-01 11:00:28 +0900

    add spring app

--- src/routes/MotionApp.svelte
+++ src/routes/MotionApp.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-  import { tweened } from 'svelte/motion';
+  import { tweened, spring } from 'svelte/motion';
   import { cubicInOut } from 'svelte/easing';
 
   const progress = tweened(0, {
     duration: 500,
     easing: cubicInOut,
   });
+
+  let ball = spring(
+    { x: 50, y: 50 },
+    {
+      stiffness: 0.1,
+      damping: 0.1,
+    },
+  );
+
+  let size = spring(10);
 </script>
 
 <progress value={$progress} class="block w-full" />
@@ -15,3 +25,21 @@
 <button on:click={() => progress.update((n) => n + 0.5)}>+50%</button>
 <button on:click={() => progress.update((n) => n + 0.75)}>+75%</button>
 <button on:click={() => progress.set(1)}>100</button>
+
+<svg
+  on:mousemove={(e) => ball.set({ x: e.clientX, y: e.clientY })}
+  on:mousedown={() => size.set(30)}
+  on:mouseup={() => size.set(10)}
+>
+  <circle cx={$ball.x} cy={$ball.y} r={$size} />
+</svg>
+
+<style>
+  svg {
+    width: 100%;
+    height: 300px;
+  }
+  circle {
+    fill: #ff3e00;
+  }
+</style>
