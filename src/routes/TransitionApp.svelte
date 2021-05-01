15717914ce32bd961a58d86d98cda87875e6ff75
miily8310s <rillakerika@gmail.com>
2021-05-01 20:19:28 +0900

    add fade app

--- src/routes/TransitionApp.svelte
+++ src/routes/TransitionApp.svelte
@@ -1,1 +1,10 @@
-aa
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+  let visible = true;
+</script>
+
+<input type="checkbox" bind:checked={visible} />
+
+{#if visible}
+  <p transition:fade>hoge hoge hoge</p>
+{/if}
