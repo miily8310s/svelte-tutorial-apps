59c1e6fd27f6b6bca2e83f07a4a95ffe289b7480
miily8310s <rillakerika@gmail.com>
2021-05-04 15:09:10 +0900

    add slot component

---
+++ src/components/BoxSlot.svelte
@@ -0,0 +1,3 @@
+<div class="border-2 border-green-400 bg-white p-2">
+  <slot />
+</div>
--- src/routes/SlotApp.svelte
+++ src/routes/SlotApp.svelte
@@ -1,1 +1,8 @@
-ddd
+<script lang="ts">
+  import BoxSlot from '../components/BoxSlot.svelte';
+</script>
+
+<BoxSlot>
+  <h1>slot game!</h1>
+  <h2 class="text-red-600 font-semibold">777!</h2>
+</BoxSlot>
