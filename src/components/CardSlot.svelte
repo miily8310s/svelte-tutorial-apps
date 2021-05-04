ae4a9066116d048e7beb52b73e05c22f9aade08b
miily8310s <rillakerika@gmail.com>
2021-05-04 15:37:37 +0900

    add slot name component

---
+++ src/components/CardSlot.svelte
@@ -0,0 +1,11 @@
+<div class="border-2 border-green-400 bg-white p-2 shadow-md lg:w-1/4 w-full">
+  <slot name="name">
+    <h2 class="font-semibold">Unknown Name</h2>
+  </slot>
+  <slot name="email">
+    <h2>Unknown Email</h2>
+  </slot>
+  <slot name="address">
+    <h2>Unknown Address</h2>
+  </slot>
+</div>
--- src/routes/SlotApp.svelte
+++ src/routes/SlotApp.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import BoxSlot from '../components/BoxSlot.svelte';
+  import CardSlot from '../components/CardSlot.svelte';
 </script>
 
 <BoxSlot>
@@ -10,3 +11,10 @@
 <p class="mt-2" />
 
 <BoxSlot />
+
+<p class="mt-2" />
+
+<CardSlot>
+  <h2 slot="name" class="font-semibold">John Doe</h2>
+  <h2 slot="email">hoge-fuga@gmail.com</h2>
+</CardSlot>
